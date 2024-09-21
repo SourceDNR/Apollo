522a49440b86c16e08baa1cc24dc3015768d65fe
source <gospodi1011+source@gmail.com>
2024-09-21 18:19:31 +0300

    Update ResizeScript.lua

--- ResizeScript.lua
+++ ResizeScript.lua
@@ -1,40 +1,39 @@
 -- this script will turn you into a giant in Apollo's game
 -- i didnt put much effort into it but its good enough
--- Temporary fix
 local size = 3
 local args = {
     [1] = {
         ["SwimAnimation"] = 0,
-        ["DepthScale"] = size,
+        ["DepthScale"] = size - (size / 5),
         ["RightLegColor"] = Color3.new(0.6392157077789307, 0.6352941393852234, 0.6470588445663452),
-        ["MoodAnimation"] = 10647852134,
+        ["MoodAnimation"] = 14618207727,
         ["Face"] = 0,
         ["JumpAnimation"] = 0,
         ["HeadColor"] = Color3.new(0.6392157077789307, 0.6352941393852234, 0.6470588445663452),
-        ["BodyTypeScale"] = 1,
+        ["BodyTypeScale"] = 0,
         ["ClimbAnimation"] = 0,
         ["LeftArmColor"] = Color3.new(0.6392157077789307, 0.6352941393852234, 0.6470588445663452),
         ["LeftLegColor"] = Color3.new(0.6392157077789307, 0.6352941393852234, 0.6470588445663452),
         ["Pants"] = 0,
         ["RightArmColor"] = Color3.new(0.6392157077789307, 0.6352941393852234, 0.6470588445663452),
-        ["WidthScale"] = size,
-        ["LeftArm"] = 14569054403,
+        ["WidthScale"] = size - (size / 5),
+        ["LeftArm"] = 17714601898,
         ["IdleAnimation"] = 0,
-        ["RightArm"] = 14569051048,
+        ["RightArm"] = 17714600336,
         ["GraphicTShirt"] = 0,
-        ["Head"] = 14569051412,
+        ["Head"] = 17714599711,
         ["Shirt"] = 0,
-        ["Torso"] = 14569054699,
+        ["Torso"] = 17714600331,
         ["RunAnimation"] = 0,
         ["WalkAnimation"] = 0,
         ["FallAnimation"] = 0,
         ["TorsoColor"] = Color3.new(0.6392157077789307, 0.6352941393852234, 0.6470588445663452),
         ["Accessories"] = {},
-        ["RightLeg"] = 14569051094,
+        ["RightLeg"] = 17714600593,
         ["HeadScale"] = size,
         ["HeightScale"] = size,
         ["ProportionScale"] = 0,
-        ["LeftLeg"] = 14569050982
+        ["LeftLeg"] = 17714599961
     }
 }
 
