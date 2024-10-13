06702d5d7806c24ee7b050ad1c75b9b9b02982c8
source <gospodi1011+source@gmail.com>
2024-10-13 13:30:31 +0300

    Update FreeCars.lua

--- FreeCars.lua
+++ FreeCars.lua
@@ -1,6 +1,7 @@
 -- This gives you free cars in apollo's game
 -- Put in autoexec folder of your executor so this script loads automatically
 repeat task.wait() until game:GetService("Players").LocalPlayer
+task.wait(1)
 local ownedCars = game:GetService("Players").LocalPlayer:WaitForChild("ownedCars")
 for i, v in ipairs(ownedCars:GetChildren()) do
     if v:IsA("BoolValue") then
